5d364c6f80a1da2a760892a802ba6fb28f7e72bd
Trần Danh <danh.trancong2002@gmail.com>
2023-11-01 02:19:22 +0700

    danh commit

--- app/Models/Order_status.php
+++ app/Models/Order_status.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Payment_methods extends Model
+class Order_status extends Model
 {
     use HasFactory;
-    protected $table = 'Payment_methods';
-    protected $primaryKey = 'PMid';
-    protected $fillable = [
-        'PMid',
-        'Payment_method_name',
-        'Discription'
-    ];
+    protected $table = 'Order_status';
+    protected $primaryKey = 'OSid';
 
+    protected $fillable = [
+        'OSid',
+        'Oid',
+        'Status'
+    ];
 }
